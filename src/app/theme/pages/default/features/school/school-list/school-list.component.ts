71e76b7ca8f0050c20988c513969b81d5fd5f4ab
suyashssc <suyash.chiplunkar@eternussolutions.com>
2017-10-25 15:05:04 +0530

    Updated

--- src/app/theme/pages/default/features/school/school-list/school-list.component.ts
+++ src/app/theme/pages/default/features/school/school-list/school-list.component.ts
@@ -6,29 +6,9 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None,
 })
 export class SchoolListComponent implements OnInit {
-  userList : any;  
   constructor() {
   }
 
   ngOnInit() {
-    this.userList = [{
-        ID: 10,
-        Name: 'admin',
-    },
-    {
-        ID: 11,
-        Name: 'user1',
-    },
-    {
-        ID: 12,
-        Name: 'user2',
-    },
-    {
-        ID: 13,
-        Name: 'user3',
-    }];
   }
-
-    onManageRoleClick(data: any) {
-    }
 }
