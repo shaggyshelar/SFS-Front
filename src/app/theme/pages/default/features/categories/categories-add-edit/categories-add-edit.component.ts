dd656a2bdf8885e94ef731e85cfef99f11b26ca5
suyashssc <suyash.chiplunkar@eternussolutions.com>
2017-10-26 13:43:03 +0530

    Added grid and form to Categories Management

--- src/app/theme/pages/default/features/categories/categories-add-edit/categories-add-edit.component.ts
+++ src/app/theme/pages/default/features/categories/categories-add-edit/categories-add-edit.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { ActivatedRoute, Router, Params } from '@angular/router';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: "app-users-list",
@@ -6,9 +8,40 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None,
 })
 export class CategoriesAddEditComponent implements OnInit {
-  constructor() {
+  errorMessage: any;
+  params: number;
+  categoriesForm: FormGroup;
+  constructor(
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute, private router: Router
+  ) {
   }
 
   ngOnInit() {
+    this.categoriesForm = this.formBuilder.group({
+      ID: [0],
+      CategoryName: ['', [Validators.required]],
+      CategoryDescription: ['', [Validators.required]],
+    });
+    // this.route.params.forEach((params: Params) => {
+    //   this.params = params['roleId'];
+    //   if (this.params) {
+    //     this.schoolForm.setValue({
+    //       ID: 1,
+    //       Name: 'SuperAdmin',
+    //     });
+    //   }
+    // });
+  }
+
+  onSubmit({ value, valid }: { value: any, valid: boolean }) {
+    if (this.params) {
+      this.router.navigate(['/features/categories/list']);
+    } else {
+      this.router.navigate(['/features/categories/list']);
+    }
+  }
+  onCancel() {
+    this.router.navigate(['/features/categories/list']);
   }
 }
