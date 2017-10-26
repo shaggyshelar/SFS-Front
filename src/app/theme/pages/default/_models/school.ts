962b821840babecdf4481ecfa36efceb0a347998
sachinsarse <sachin.sarse@eternussolutions.com>
2017-10-26 19:02:01 +0530

    Updated School Management with api service

---
+++ src/app/theme/pages/default/_models/school.ts
@@ -0,0 +1,10 @@
+export class School {
+    id: number;
+    SchoolId: number;
+    InstituteId: number;
+    SchoolName: string;
+    SchoolCode: string;
+    SchoolEmail: string;
+    SchoolPhone: string;
+    SchoolAddress: string;
+}
