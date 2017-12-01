7dbd5c621885ee98b7be868a841f6d7564fa13c2
Sagar Shelar <shaggy.shelar@gmail.com>
2017-12-01 11:48:25 +0530

    Fixed issue related to parsing response into JSON first

--- src/app/_services/error-handler.service.ts
+++ src/app/_services/error-handler.service.ts
@@ -43,7 +43,7 @@ export class GlobalErrorHandler implements ErrorHandler {
       }
     } else {
       console.error(response);
-      this.messageService.addMessage({ severity: 'error', summary: 'Failed', detail: response.Message });
+      this.messageService.addMessage({ severity: 'error', summary: 'Failed', detail: response.json().Message });
     }
   }
 }
