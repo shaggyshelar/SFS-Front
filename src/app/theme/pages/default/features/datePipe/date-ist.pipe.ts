c493d5ed53d14f6511d2df80ff4d934a04541e6f
harshmatic <harshit.jyoti@eternussolutions.com>
2018-03-29 18:17:03 +0530

    filter issue, date changes enhancements

--- src/app/theme/pages/default/features/datePipe/date-ist.pipe.ts
+++ src/app/theme/pages/default/features/datePipe/date-ist.pipe.ts
@@ -7,7 +7,6 @@ export class DateIst implements PipeTransform {
 
   transform(date: string): any {
     date = date ? date.split('T')[0] : '';
-    console.log(date);
     return date==''?null:new Date(date);
   }
 
